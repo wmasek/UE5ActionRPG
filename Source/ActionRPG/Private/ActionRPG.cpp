9d80df10781786303300331558f174e0f25325a9
Will Masek <willmasek@gmail.com>
2023-07-14 12:47:58 +0100

    Removal, no engine.

--- Source/ActionRPG/Private/ActionRPG.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ActionRPG.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ActionRPG, "ActionRPG" );
-
-/** Logging definitions */
-DEFINE_LOG_CATEGORY(LogActionRPG);
